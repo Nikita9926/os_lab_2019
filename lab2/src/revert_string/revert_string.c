5e8374a51918968af9804dd3a8a4389db62ef72e
nikita19992022 <nikita19992022@gmail.com>
2020-04-11 10:52:05 +0000

    добаÐсделаны 3 заданияÐ

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "revert_string.h"
 
-void RevertString(char *str)
-{
-	// your code here
+extern void RevertString(char *str) {
+	int len = strlen(str); 
+    int i;
+    for(i = 0; i < len / 2; i++) {
+        char temp = str [i];
+        str [i] = str [len-i-1]; 
+        str [len-i-1] = temp;
+    }
 }
 
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -2,5 +2,7 @@
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+	char cont = *left;
+    *left = *right;
+    *right = cont;
 }
