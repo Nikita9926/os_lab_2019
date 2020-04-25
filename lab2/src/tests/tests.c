342f03a4b932a030cca11c8db2e5f47f508f287f
nikita19992022 <nikita19992022@gmail.com>
2020-04-25 11:55:45 +0000

    tests

--- lab2/src/tests/tests.c
+++ lab2/src/tests/tests.c
@@ -1,6 +1,6 @@
-#include <CUnit/Basic.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 #include "revert_string.h"
 
@@ -11,42 +11,20 @@ void testRevertString(void) {
   char str_with_even_chars_num[] = "abcd";
 
   RevertString(simple_string);
-  CU_ASSERT_STRING_EQUAL_FATAL(simple_string, "olleH");
+  assert(0 == strcmp(simple_string, "olleH"));
 
   RevertString(str_with_spaces);
-  CU_ASSERT_STRING_EQUAL_FATAL(str_with_spaces, "secaps htiw gnirtS");
+  assert(0 == strcmp(str_with_spaces, "secaps htiw gnirtS"));
 
   RevertString(str_with_odd_chars_num);
-  CU_ASSERT_STRING_EQUAL_FATAL(str_with_odd_chars_num, "cba");
+  assert(0 == strcmp(str_with_odd_chars_num, "cba"));
 
   RevertString(str_with_even_chars_num);
-  CU_ASSERT_STRING_EQUAL_FATAL(str_with_even_chars_num, "dcba");
+  assert(0 == strcmp(str_with_even_chars_num, "dcba"));
+
 }
 
 int main() {
-  CU_pSuite pSuite = NULL;
-
-  /* initialize the CUnit test registry */
-  if (CUE_SUCCESS != CU_initialize_registry()) return CU_get_error();
-
-  /* add a suite to the registry */
-  pSuite = CU_add_suite("Suite", NULL, NULL);
-  if (NULL == pSuite) {
-    CU_cleanup_registry();
-    return CU_get_error();
-  }
-
-  /* add the tests to the suite */
-  /* NOTE - ORDER IS IMPORTANT - MUST TEST fread() AFTER fprintf() */
-  if ((NULL == CU_add_test(pSuite, "test of RevertString function",
-                           testRevertString))) {
-    CU_cleanup_registry();
-    return CU_get_error();
-  }
-
-  /* Run all tests using the CUnit Basic interface */
-  CU_basic_set_mode(CU_BRM_VERBOSE);
-  CU_basic_run_tests();
-  CU_cleanup_registry();
-  return CU_get_error();
+  testRevertString();
+  return 0;
 }
